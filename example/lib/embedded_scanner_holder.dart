0c46487267d66ead313b61b438a1910d5227a99c
louis <laixueqiang@126.com>
2020-11-07 16:49:31 +0800

    example

--- example/lib/embedded_scanner_holder.dart
+++ example/lib/embedded_scanner_holder.dart
@@ -16,69 +16,57 @@ class EmbeddedScannerHolder extends StatefulWidget {
 }
 
 class _EmbeddedScannerHolderState extends State<EmbeddedScannerHolder> with SingleTickerProviderStateMixin {
-  bool visible = false;
-  bool flashOn = false;
-  bool okToScan = true;
+  AnimationController _animtionController;
+  Animation<double> _tween;
+
+  int _maxScan;
+  int _delay;
+  bool _autoRestart;
+  List<int> _formats;
+
+  bool _visible = false;
+  bool _flashOn = false;
+  bool _okToScan = true;
+  EmbeddedScannerController _controller;
+
+
   Function(String) onCode;
-  EmbeddedScannerController controller;
+  VoidCallback onFinish;
 
-  AnimationController animtionController;
-  Animation<double> tween;
-
-  int maxScan;
-  int delay;
-  bool autoRestart;
-  List<int> formats;
+  _EmbeddedViewState _viewState;
 
   @override
   initState() {
     super.initState();
 
-    animtionController = AnimationController(duration: Duration(milliseconds: 150), vsync: this);
-    animtionController.addStatusListener((v) {
-      if (v == AnimationStatus.completed) {
+    _animtionController = AnimationController(duration: Duration(milliseconds: 150), vsync: this);
+    _animtionController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
         setState(() {
-          visible = true;
-          okToScan = true;
-          flashOn = false;
+          _visible = true;
+          _flashOn = false;
+          _okToScan = true;
         });
       }
     });
-    tween = Tween(begin: 0.0, end: 200.0).animate(animtionController);
+    _tween = Tween(begin: 0.0, end: 200.0).animate(_animtionController);
   }
 
-  show({int maxScan, int delay, bool autoRestart=false, List<int> formats}) {
-    this.maxScan = maxScan;
-    this.delay = delay;
-    this.autoRestart = autoRestart;
-    this.formats = formats;
-
-    animtionController.forward();
-  }
-
-  hide() {
+  _onReady() {
     setState(() {
-      visible = false;
-      maxScan = null;
-      delay = null;
-      autoRestart = null;
-      formats = null;
-    });
-
-    Future.delayed(Duration(milliseconds: 100)).then((_) {
-      animtionController.reverse();
+      _okToScan = true;
     });
   }
 
-  onReady() {
+  _onToggleFlash(bool on) {
     setState(() {
-      okToScan = true;
+      _flashOn = on;
     });
   }
 
-  onResult(String code) {
+  _onResult(String code) {
     setState(() {
-      okToScan = false;
+      _okToScan = false;
     });
 
     if (onCode != null) {
@@ -86,10 +74,59 @@ class _EmbeddedScannerHolderState extends State<EmbeddedScannerHolder> with Sing
     }
   }
 
-  onToggleFlash(bool on) {
-    setState(() {
-      flashOn = on;
+  show({int maxScan, int delay, bool autoRestart=false, List<int> formats}) async {
+    this._maxScan = maxScan;
+    this._delay = delay;
+    this._autoRestart = autoRestart;
+    this._formats = formats;
+
+    if (_viewState == null) {
+      _animtionController.forward();
+      await _EmbeddedView.popup(context, onTap: () {
+        _controller?.readyToScan();
+      });
+
+      // popup 紧跟着 finish
+      _finish();
+    } else {
+      // 只是设置参数
+      _setScanParams();
+    }
+  }
+
+  hide() {
+    if (_viewState != null) {
+      // dismiss 会结束 show 中的阻塞，然后在 show 中调用 finish
+      _viewState.dismiss();
+    }
+  }
+
+  _finish() {
+    if (onFinish != null) {
+      onFinish();
+    }
+
+    Future.delayed(Duration(milliseconds: 100)).then((_) {
+      _animtionController.reverse();
     });
+
+
+    onCode = null;
+    onFinish = null;
+    _viewState = null;
+
+    setState(() {
+      _visible = false;
+    });
+  }
+
+  _setScanParams() {
+    _controller.setScanParams(
+      delay: _delay,
+      maxScan: _maxScan,
+      autoRestart: _autoRestart,
+      formats: _formats,
+    );
   }
 
   @override
@@ -102,47 +139,40 @@ class _EmbeddedScannerHolderState extends State<EmbeddedScannerHolder> with Sing
       children: <Widget>[
         Stack(
           children: <Widget>[
-            Stack(
-              children: <Widget>[
-                AnimatedBuilder(
-                  animation: tween,
-                  child: Container(),
-                  builder: (context, Widget child) {
-                    return Container(
-                      height: tween.value,
-                    );
-                  }
-                ),
-
-                visible ?
-                Positioned(
-                  child: Container(
-                    height: 200,
-                    child: EmbeddedScanner(
-                      onCreated: (controller) async {
-                        this.controller = controller;
-                        await controller.setScanParams(
-                          delay: delay,
-                          maxScan: maxScan,
-                          autoRestart: autoRestart,
-                          formats: formats,
-                        );
-                        setState(() {
-                          controller.onReady = onReady;
-                          controller.onCode = onResult;
-                          controller.onFinish = hide;
-                          controller.onToggleFlash = onToggleFlash;
-                        });
-                      },
-                    )
-                  ),
-                ) : Container(),
-
-              ],
+            AnimatedBuilder(
+              animation: _tween,
+              child: Container(),
+              builder: (context, Widget child) {
+                return Container(
+                  height: _tween.value,
+                );
+              }
             ),
 
+            // camera view
+            _visible ?
+            Positioned(
+              child: Container(
+                width: screenSize.width,
+                height: 200,
+                child: EmbeddedScanner(
+                  onCreated: (controller) async {
+                    this._controller = controller;
+                    setState(() {
+                      controller.onReady = _onReady;
+                      controller.onCode = _onResult;
+                      controller.onFinish = hide;
+                      controller.onToggleFlash = _onToggleFlash;
+                    });
+
+                    _setScanParams();
+                  },
+                )
+              ),
+            ) : Container(),
+
             // indicator
-            visible && okToScan ?
+            _visible && _okToScan ?
             Positioned(
               top: 35,
               right: 10,
@@ -158,7 +188,7 @@ class _EmbeddedScannerHolderState extends State<EmbeddedScannerHolder> with Sing
             ) : Container(),
 
             // close button
-            visible ?
+            _visible ?
             Positioned(
               top: 30,
               left: 10,
@@ -177,12 +207,12 @@ class _EmbeddedScannerHolderState extends State<EmbeddedScannerHolder> with Sing
             ) : Container(),
 
             // flash button
-            visible ?
+            _visible ?
             Positioned(
               left: 10,
               bottom: 10,
               child: GestureDetector(
-                onTap: controller?.toggleFlash,
+                onTap: _controller?.toggleFlash,
                 child: Container(
                   width: 46,
                   height: 46,
@@ -190,7 +220,7 @@ class _EmbeddedScannerHolderState extends State<EmbeddedScannerHolder> with Sing
                     color: Color(0xFF383838),
                     borderRadius: BorderRadius.all(Radius.circular(23)),
                   ),
-                  child: Icon(flashOn ? Icons.flash_on : Icons.flash_off, color: Colors.white, size: 25),
+                  child: Icon(_flashOn ? Icons.flash_on : Icons.flash_off, color: Colors.white, size: 25),
                 ),
               ),
             ) : Container(),
@@ -200,29 +230,80 @@ class _EmbeddedScannerHolderState extends State<EmbeddedScannerHolder> with Sing
 
         Expanded(
           flex: 1,
-          child: Stack(
-            children: <Widget>[
-              visible ?
-              WillPopScope(
-                onWillPop: () async {
-                  hide();
-                  return false;
-                },
-                child: widget.child,
-              ) : widget.child,
+          child: widget.child,
+        ),
+      ],
+    );
+  }
+}
 
-              visible ?
-              Positioned(
-                child: GestureDetector(
-                  onTap: controller?.readyToScan,
-                  child: Container(
-                    height: screenSize.height-tween.value,
-                    color: Color(0x3F000000),
-                  ),
-                )
-              ) : Container(),
+// ----------------------------------------------------------------------------------------
 
-            ],
+class _EmbeddedView extends StatefulWidget {
+  final Function onTap;
+  final Function onCreate;
+
+  _EmbeddedView({
+    this.onTap,
+    this.onCreate,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _EmbeddedViewState();
+  }
+
+  static popup(BuildContext context, {Function onTap, Function onCreate}) {
+    return showGeneralDialog(
+      context: context,
+      barrierColor: Color(0x00FFFFFF),
+      barrierDismissible: false,
+      transitionDuration: Duration(milliseconds: 150),
+      transitionBuilder: (a,b,c, child) {
+        return b.isCompleted ? child : Container();
+      },
+      pageBuilder: (context, Animation<double> animation1, Animation<double> animation2) {
+        return _EmbeddedView(onTap: onTap, onCreate: onCreate);
+      }
+    );
+  }
+}
+
+class _EmbeddedViewState extends State<_EmbeddedView> {
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.onCreate != null) {
+      widget.onCreate(this);
+    }
+  }
+
+  void dismiss() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: GestureDetector(
+            onTap: widget.onTap,
+            child: Container(
+              height: screenSize.height-200,
+              color: Color(0x3F000000),
+            ),
           ),
         ),
       ],
